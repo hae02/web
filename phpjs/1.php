34d369da9706bf151d926969a7c362c18bb4f50e
HAE\cnh02 <cnh020613@naver.com>
2022-11-14 23:54:55 +0900

    php,java script 이론
    php, java script 공부

---
+++ phpjs/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo 10+10; ?>
+    <h1>JavaScript</h1>
+    <script>
+      document.write(10+10);
+
+    </script>
+  </body>
+</html>
