0faedb8fdaad780178f7b8600c0b02abe6b2c43a
HAE\cnh02 <cnh020613@naver.com>
2022-11-15 23:58:03 +0900

    javascipt 로그인 기능
    prompt => 로그인창 나옴

---
+++ phpjs/6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      password = prompt("비밀번호");
+      if(password ==1111){
+        document.write("안녕하세요.");
+      }
+      else{
+        document.write("비밀번호가 틀렸습니다.");
+      }
+    </script>
+  </body>
+</html>
