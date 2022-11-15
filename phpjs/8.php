a5c029ba04f208dff19ecdbed5a554826737c571
HAE\cnh02 <cnh020613@naver.com>
2022-11-16 00:02:32 +0900

    javascript, php 반복문
    while안에 있는 조건에 만족할때까지 반복함

---
+++ phpjs/8.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <ul>
+    <script type="text/javascript">
+      i = 0;
+      while (i < 10) {
+        document.write("<li>hello world</li>");
+        i = i+1;
+      }
+    </script>
+    </ul>
+    <h1>php</h1>
+    <?php
+      $i = 0;
+      while($i < 10){
+        echo "hello world<br />";
+        $i = $i +1;
+      }
+     ?>
+  </body>
+</html>
