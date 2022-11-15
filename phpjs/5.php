3eee382bc433fece376a44d8c4c1c8339b99fb09
HAE\cnh02 <cnh020613@naver.com>
2022-11-15 23:56:31 +0900

    php,javascript 조건문
    if, else

---
+++ phpjs/5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script charset="utf-8">
+      result = (1==1);
+      if(result){
+        document.write("참");
+      }
+      else{
+        document.write("거짓");
+      }
+    </script>
+    <h1>php</h1>
+    <?php
+
+    ?>
+  </body>
+</html>
