ca5171215f00fa010ae133b770c8bc669ce8789c
HAE\cnh02 <cnh020613@naver.com>
2022-11-16 23:11:49 +0900

    함수의 기본문법
    function function_name(){
    }
    function_name();

---
+++ phpjs/11.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script type="text/javascript">
+      function a(){
+        document.write("Hello JavaScript Function");
+      }
+      a();
+    </script>
+
+    <h1>php</h1>
+    <?php
+      function a(){
+        echo "Hello PHP Function";
+      }
+      a();
+    ?>
+  </body>
+</html>
